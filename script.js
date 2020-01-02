797df09e625d6f3a93909baf8ffc586ed7dd1a2c
Teresa Liu <kuas033@gmail.com>
2020-01-01 20:23:06 -0800

    date added

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+var now = moment().format('MMMM Do YYYY');
+var nowHour24 = moment().format('H');
+var nowHour12 = moment().format('h');
+
+var $dateHeading = $('#time');
+  $dateHeading.text(now);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
