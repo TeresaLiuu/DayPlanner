6e2fc10e5af0da0dc04b342c6afe050849e24868
Teresa Liu <kuas033@gmail.com>
2020-01-03 20:19:54 -0800

    change textarea color by time

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 $(document).ready(function () {
 
-var now = moment().format('MMMM Do YYYY');
-var nowHour24 = moment().format('H');
-var nowHour12 = moment().format('h');
+  var now = moment().format('MMMM Do YYYY');
+  var dateHeading = $('#time');
+  dateHeading.text(now);
 
-var $dateHeading = $('#time');
-  $dateHeading.text(now);
+  var dt = new Date();
+  var currentHour = dt.getHours();
+  var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-  $("button").click(function(){
+  $('.hourRow').text(workHours[0]);
 
+  for (var i = 0; i < workHours.length; i++) {
+    var hourDiv = $('.hourRow').eq(i);
+    hourDiv.text(workHours[i]);
+
+    var inputElement = $('.form-control').eq(i);
+
+    if (currentHour < workHours[i]) {
+      inputElement.css("background-color", "pink");
+    }
+    else if (currentHour === workHours[i]) {
+      inputElement.css("background-color", "grey");
+    }
+    else {
+      inputElement.css("background-color", "yellow");
+    }
+
+  }
+
+  $('button').click(function () {
+
+  })
 
 
 });
+
+
+
+
