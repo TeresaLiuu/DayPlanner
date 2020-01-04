38a917e51cc2051cf5c6d3255ca4dc6191ff1283
Teresa Liu <kuas033@gmail.com>
2020-01-03 22:26:34 -0800

    add and retrieve value from local storage

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # HW-5-DayPlanner
+## Day Planner Functions:
+
+A simple calendar that allows the user to save events for each hour of the day. This will run in the browser and feature dynamically updated HTML and CSS powered by jQuery. This day planner display standard business hours (9 a.m. to 5 p.m.). Each time slot should represent one hour and contain the following:
+
+1. The time
+
+2. Input schedule
+
+3. Save button
+
+Clicking on the save button will store the time and user input in localStorage. 
+
+Near the top of the calendar, it displays the current day. Additionally, input schedule text box will change color according to current time. 
+
+-- If the time has passed current time, the text box will turn into pink.
+
+-- If the time is current time, the text box will turn into grey.
+
+-- If the time is future time, the text box will turn into yellow.
+
+
--- script.js
+++ script.js
@@ -6,15 +6,15 @@ $(document).ready(function () {
 
   var dt = new Date();
   var currentHour = dt.getHours();
-  var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+  var workHours = ['9 am', '10 am', '11 am', '12 pm', '13 pm', '14 pm', '15 pm', '16 pm', '17 pm'];
+  
+  
 
-  $('.hourRow').text(workHours[0]);
-
-  for (var i = 0; i < workHours.length; i++) {
+  for (var i = 0; i < workHours.length; ++i) {
     var hourDiv = $('.hourRow').eq(i);
     hourDiv.text(workHours[i]);
 
-    var inputElement = $('.form-control').eq(i);
+    var inputElement = $('.form-control').eq(i); 
 
     if (currentHour < workHours[i]) {
       inputElement.css("background-color", "pink");
@@ -26,12 +26,21 @@ $(document).ready(function () {
       inputElement.css("background-color", "yellow");
     }
 
+    var buttonElement = $('.btn-primary').eq(i); 
+    buttonElement.click(clickCallback(i));
+
+    // Retrieve saved value
+    var savedValue = localStorage.getItem(workHours[i]);
+    inputElement.val(savedValue);
   }
-
-  $('button').click(function () {
-
-  })
-
+  
+  function clickCallback(i){
+    return function(){
+      var inputElement = $('.form-control').eq(i); 
+      var storedPlan = inputElement.val();
+      localStorage.setItem(workHours[i], storedPlan);
+    }
+  }
 
 });
 
